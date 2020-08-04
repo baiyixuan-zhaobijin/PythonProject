0c1abe6cbb7b48c106a05eb513e1de8548ac4736
zhaobijin <baiyixuanzhaobijin@163.com>
2020-08-04 10:28:09 +0800

    数据处理

--- DataOper/doctor-20-08-03.py
+++ DataOper/doctor-20-08-03.py
@@ -1,5 +1,6 @@
 from peewee import *
 import csv
+import pandas as pd
 fileName = 'D:\wamp64\www\PythonProject\DataOper\doctor.csv'
 db = MySQLDatabase("doctor_exam", host="127.0.0.1", port=3306, user="root", passwd="123456")
 db.connect()
@@ -19,17 +20,23 @@ class doctors_test(BaseModel):
 class doctors(BaseModel):
     name = CharField()
     hospital = CharField()
+    telphone = CharField()
+    department = CharField()
 
-result = {}
+rlist  = []
+n      = 0
 with open(fileName) as f:
     reader   = csv.reader(f)
     head_row = next(reader)
     for row in reader:
-        result['name'] = row[0]
-        result['province'] = row[1]
-        result['city'] = row[2]
-        result['hospital']   = row[3]
-        result['department'] = row[7]
+        result = []
+        result.append(row[0])
+        #result['province'] = row[1]
+        #result['city'] = row[2]
+        result.append(row[3])
+        result.append(row[7])
+        result.append(row[8])
+        '''
         result['job'] = '4'
         if row[8].strip() == '主任医师':
             result['job']        = '5'
@@ -45,6 +52,25 @@ with open(fileName) as f:
             result['job']        = '1'
         elif row[8].strip() == '住院医师':
             result['job']        = '3'
-        if len(doctors.select().where(doctors.name == row[0],doctors.hospital == row[3])) == 0:
-            doctors_test.insert(result).execute()
-            print(result)
+        '''
+        hospital = row[3]
+        if hospital.find('(')>0:
+            index = hospital.find('(')
+            hospital = hospital[0:index]
+        elif hospital.find('（')>0:
+            index = hospital.find('（')
+            hospital = hospital[0:index]
+        if len(doctors.select().where(doctors.name == row[0],doctors.hospital == hospital)) > 0:
+            doctor = doctors.get(doctors.name == row[0],doctors.hospital == hospital)
+
+            result.append(str(doctor.telphone.strip()))
+            result.append( str(doctor.department.strip()))
+            #print(result)
+            rlist.append(result)
+           
+            
+
+    column = ['姓名','医院','执业范围','职称','电话','科室']
+    test = pd.DataFrame(columns = column, data=rlist)
+    test.to_csv('D:/test.csv') 
+    print('任务完成')
--- Spider/test2.py
+++ Spider/test2.py
@@ -1,5 +1,6 @@
-from urllib import request
-response = request.urlopen(r'https://jixi.58.com/ershouche/pn1/?PGTID=0d100000-01c7-92a6-616f-3afaed1bae9c&ClickID=57/') # <http.client.HTTPResponse object at 0x00000000048BC908> HTTPResponse类型
-page = response.read()
-page = page.decode('utf-8')
-print(page)
+list1 = [1,2,3]
+list3 = [1,2,3]
+list2 = []
+list2.append(list1)
+list2.append(list3)
+print(list2)

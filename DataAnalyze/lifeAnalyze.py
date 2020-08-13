7b8acf5bc726408497907a7f2036d8ab486af949
zhaobijin <baiyixuanzhaobijin@163.com>
2020-08-13 15:08:08 +0800

    功能优化

--- DataAnalyze/lifeAnalyze.py
+++ DataAnalyze/lifeAnalyze.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import matplotlib.pyplot  as plt
-import datetime
+import matplotlib.pyplot as plt
 import common
 import arrow
 date = arrow.utcnow().format('YYYY-MM-DD')
@@ -11,9 +10,22 @@ df1 = df[(df.状态 == '✔')&(df.日期==date)]
 df2 = df1.loc[:,['类型','用时']]
 df2['用时'] =  [int(x.replace('min','').strip()) for x in df2['用时']]
 df3 = df2.groupby(['类型']).sum().reset_index()
+df3 = df3.sort_values(by='用时')
 plt.rcParams['font.sans-serif']=['SimHei']
-plt.title(label='总计用时：'+ common.display_time(sum(df3['用时'])*60))
+title = '总计用时：'+ common.display_time(sum(df3['用时'])*60)
+plt.title(label=title)
 x_data = df3['类型']
 y_data = df3['用时']
-common.autolabel(plt.bar(range(len(y_data)), y_data, color='rgb', tick_label=x_data))
+type = input()
+if(type == '2'):
+    plt.rcParams['axes.unicode_minus'] = False
+    plt.barh(range(x_data.count()), y_data, height=0.7, color='steelblue', alpha=0.8)      # 从下往上画
+    plt.yticks(range(x_data.count()), x_data)
+    plt.xlabel("用时")
+    for x, y in enumerate(y_data):
+        plt.text(y + 0.2, x - 0.1, '%s' % y)
+elif(type == '1'):
+    plt.pie(y_data,labels=x_data,autopct='%1.1f%%',shadow=False,startangle=150)
+elif(type == '3'):
+   common.autolabel(plt.bar(range(len(y_data)), y_data, color='rgb', tick_label=x_data))
 plt.show() 
--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 
 import pandas as pd
 import arrow
+date = arrow.utcnow().format('YYYY-MM-DD')
 fileName = 'J:\\我的坚果云\\时计-20-08-12.xlsx'
 df = pd.read_excel(fileName,'时计')
 df['日期'] = pd.to_datetime(df['日期'],format="%Y-%m-%d")
-print(df[(df.状态=='✔')&(df.日期=='2020-08-13')])
-date = arrow.utcnow().format('YYYY-MM-DD')
-print(date)
+df1 = df[(df.状态 == '✔')&(df.日期==date)]
+df2 = df1.loc[:,['类型','用时']]
+df2['用时'] =  [int(x.replace('min','').strip()) for x in df2['用时']]
+df3 = df2.groupby(['类型']).sum().reset_index()
+df3 = df3.sort_values(by='用时')
+print(df3)
+
